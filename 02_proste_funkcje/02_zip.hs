58fa30129922685bcb78f2e0b63c367b59901e2e
Marta <martaswierczewska@wp.pl>
2020-03-30 08:30:49 +0000

    Update 02_zip.hs

--- 02_proste_funkcje/02_zip.hs
+++ 02_proste_funkcje/02_zip.hs
@@ -5,3 +5,6 @@ Podpowiedzi:
 * są trzy przypadki: pierwsza lista jest pusta, druga lista jest pusta, obie są niepuste
 -}
 zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
