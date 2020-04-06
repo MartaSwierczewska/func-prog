42e7451d6eaac2d0ef2abe1e4f4534b4ccd1c422
Marta <martaswierczewska@wp.pl>
2020-04-06 09:22:22 +0200

    1-5 done

--- 03_funkcje_wyzszego_rzedu/02_generatorOperator.hs
+++ 03_funkcje_wyzszego_rzedu/02_generatorOperator.hs
@@ -1,3 +1,5 @@
 generatorOperator :: (lewa -> prawa -> wynik) -> lewa -> (prawa -> wynik)
 -- funkcja przyjmuje operator, który jest typu (lewa -> prawa -> wynik)
 -- oraz lewą część operatora i zwraca funkcję, która przyjmuje prawą część operatora i zwraca wynik
+
+generatorOperator lewa f prawa = lewa f prawa
--- 03_funkcje_wyzszego_rzedu/05_pierwsze.hs
+++ 03_funkcje_wyzszego_rzedu/05_pierwsze.hs
@@ -1,2 +1,12 @@
+is_prime :: Int -> Bool
+is_prime 1 = False
+is_prime 2 = True
+is_prime n 
+            | n<=0 = False
+            | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False
+		    | otherwise = True
+
 pierwsze :: [Int] -> [Int]
--- funkcja znajdujące w podanej liście wszystkie liczby pierwsze
+-- funkcja znajdująca w podanej liście wszystkie liczby pierwsze
+pierwsze lista = filter is_prime lista
+
