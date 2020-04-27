1945cf16c136884446c5174cbb125ada62db3ce6
Marta <martaswierczewska@wp.pl>
2020-04-27 11:00:36 +0200

    zad 4

--- 05_io/04_spellchecker.hs
+++ 05_io/04_spellchecker.hs
@@ -4,7 +4,5 @@ import Control.Monad
 main = do     
     contents <- readFile "file-zad4"
     dictionary <- readFile "/usr/share/dict/words" 
-    list <- sequence contents  
-    filter (`notElem` dictionary) list
-    writeFile "not-in-dict-file" list
+    writeFile "not-in-dict-file" $ filter (`notElem` dictionary) contents
 
