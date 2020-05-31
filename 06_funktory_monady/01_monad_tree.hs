76b8fd4866d85b0d20285c5e95c9eef2a8188a25
Marta <martaswierczewska@wp.pl>
2020-05-31 20:52:08 +0200

    functor, monad added

--- 06_funktory_monady/01_monad_tree.hs
+++ 06_funktory_monady/01_monad_tree.hs
@@ -4,3 +4,14 @@
 -- Dodatkowo można tu zrealizować dodatkowe zadanie 4.
 
 data Tree a = Empty | Leaf a | Node (Tree a) (Tree a)
+
+instance Functor Tree where
+ fmap f Empty = Empty
+ fmap f (Leaf x) = Leaf (f x)
+ fmap f (Node x y) = Node (fmap f x) (fmap f y)
+
+instance Monad Tree where
+ return = Leaf
+ Empty >>= f = Empty 
+ Leaf a >>= f = f a
+ Node a b >>= f = Node (a >>= f) (b >>= f)
